2201f806dfb36a9dc318a52111de944b4fe37d17
rkukuh <>
2022-03-03 15:22:02 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,25 @@
+let 日本: String = "Japan"
+print(日本)
+
+
+// let 🏹: String = "Marksman"
+// print(🏹)
+
+
+// var isBanned = false
+// print(isBanned)
+
+
+
+// var winRate: Double = 45.36
+// print(winRate)
+
+// winRate = 52.76
+// print(winRate)
+
+
+
+// let heroName: String = "Tigreal"
+// print(heroName)
+
+// heroName = "Rafaela"

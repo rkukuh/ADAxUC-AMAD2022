a800d534c6bcabbeda077f287200e190ed921ee5
R. Kukuh <rkukuh@gmail.com>
2022-03-08 01:47:31 +0000

    Operators and Control Flows

--- main.swift
+++ main.swift
@@ -1,27 +1,118 @@
-let 日本: String = "Japan"
-print(日本)
+
+/// Ternary Operator
+
+//let temperature = 70
+//var result: String
+//
+//result = (temperature >= 65) && (temperature <= 75) ? "Temperature is OK" : "Temperature too hot/cold"
+//
+//print(result)
+
+
+//var largestNumber: Int
+//
+//let numberA = 40
+//let numberB = 5
+//
+////if numberA > numberB {
+////    largestNumber = numberA
+////} else {
+////    largestNumber = numberB
+////}
+//
+//// Ini adalah bentuk dari ternary operator
+//largestNumber = (numberA > numberB) ? numberA : numberB
+//
+//// String literal
+//print("Largest number is: \(largestNumber)")
 
 
 
-// let 🏹: String = "Marksman"
-// print(🏹)
+// Math operators: *, +, -, %
+// Bool operators: &&, ||, !
+// Ternary operators: ?:
 
 
 
-// var isBanned = false
-// print(isBanned)
+//let letter: Character = "u"
+//
+//switch letter {
+//case "a", "i", "u", "e", "o":
+//    print("The letter is a vowel") 
+//default:
+//    print("The letter is a consonant") 
+//}
 
 
 
-// var winRate: Double = 45.36
-// print(winRate)
+// Macam-macam tipe data integer di Swift
 
-// winRate = 52.76
-// print(winRate)
+//print(Int8.max)
+//print(Int16.max)
+//print(Int32.max)
+//print(Int.max)
+//print(Int64.max)
 
 
 
-// let heroName: String = "Tigreal"
-// print(heroName)
+/// Range Operator
 
-// heroName = "Rafaela"
+//let distance: Int = 20
+//
+//switch distance {
+//case 0...9:
+//    print("Your destination is close")
+//case 10...99:
+//    print("Your destination is not that far")
+//case 100...Int.max:
+//    print("Your destination is far from here")
+//default:
+//    print("Where are you going?") 
+//}
+//
+//
+//
+//var numberOfWheels = 2
+//
+//switch numberOfWheels {
+//case 0:
+//    print("Missing wheels")
+//case 1:
+//    print("Unicycle")
+//case 2:
+//    print("Bicycle")
+//default:
+//    print("That's a lot of wheels")
+//}
+
+
+
+//var isRain = false
+//
+//if !isRain {
+//    print("Bring your umbrella")
+//}
+
+
+
+// Booelan operator: AND (&&), OR (||), NOT (!)
+
+//var temperature = 75
+//
+//if (temperature >= 65) && (temperature <= 75) {
+//    print("Temperature is okay")
+//} else if temperature < 65 {
+//    print("It is too cold")
+//} else {
+//    print("It is too hot")
+//}
+
+
+
+//var temperature = 100
+//
+//if temperature >= 100 {
+//    print("The water is boiling")
+//} else {
+//    print("The water is not boiling")
+//}
